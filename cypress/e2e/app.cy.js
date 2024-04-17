2dd5d619890380f11213fd6c27806802d4de03f3
Igor <igor.gds.00@gmail.com>
2024-04-17 19:02:46 -0300

    Implementando testes para o 1º cenário

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -1,5 +1,78 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('/')
+import assert from 'assert'
+
+class RegisterForm {
+  elements = {
+    titleInput: () => cy.get('#title'),
+    titleFeedbackInput: () => cy.get('#titleFeedback'),
+    imageUrlInput: () => cy.get('#imageUrl'),
+    urlFeedbackInput: () => cy.get('#urlFeedback'),
+    submitBtn: () => cy.get('#btnSubmit')
+  }
+
+  typeTitle(text) {
+    if (!text) return
+    this.elements.titleInput().type(text)
+  }
+
+  typeUrl(text) {
+    if (!text) return
+    this.elements.imageUrlInput().type(text)
+  }
+
+  clickSubmit() {
+    this.elements.submitBtn().click()
+  }
+}
+
+const registerForm = new RegisterForm()
+const colors = {
+  errors: 'rgb(220, 53, 69)',
+  success: ''
+}
+
+describe('Image Registration', () => {
+  describe('Image Registration', () => {
+
+    after(() => {
+      cy.clearAllLocalStorage()
+    })
+
+    const input = {
+      title: '',
+      url: ''
+    }
+
+    it('Given I am on the image registration page', () => {
+      cy.visit('/')
+    })
+
+    it(`When I enter "${input.title}" in the title field`, () => {
+      registerForm.typeTitle(input.title)
+    })
+    
+    it(`Then I enter "${input.url}" in the URL field`, () => {
+      registerForm.typeUrl(input.url)
+    })
+
+    it(`Then I click the submit button`, () => {
+      registerForm.clickSubmit()
+    })
+
+    it(`Then I should see "Please type a title for the image" message above the title field`, () => {
+      registerForm.elements.titleFeedbackInput().should('contains.text', 'Please type a title for the image')
+    })
+
+    it(`And I should see "Please type a valid URL" message above the imageUrl field`, () => {
+      registerForm.elements.urlFeedbackInput().should('contains.text', 'Please type a valid URL')
+    })
+
+    it(`And I should see an exclamation icon in the title and URL fields`, () => {
+      registerForm.elements.titleInput().should(([element]) => {
+        const styles = window.getComputedStyle(element)
+        const border = styles.getPropertyValue('border-right-color')
+        assert.strictEqual(border, colors.errors)
+      })
+    })
+
   })
 })
